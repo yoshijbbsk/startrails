26c18b314739b59a07fd971f9063f6737a3694c1
高山　善成 <takayamayoshinari@kouzanyoshinarinoMacBook-Air.local>
2019-02-19 02:18:19 +0900

    start everip

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,13 @@
 class StaticPagesController < ApplicationController
   def home
+      puts "YO HOMMIE"
+      puts request.remote_ip
+      puts "WHATS UP"
+
       if logged_in?
-    @micropost  = current_user.microposts.build
-    @feed_items = current_user.feed.paginate(page: params[:page])
-  end
+        @micropost  = current_user.microposts.build
+        @feed_items = current_user.feed.paginate(page: params[:page])
+      end
      end
 
   def help
